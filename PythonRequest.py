117f9fd80e285d177848d2761d8806afb83ef77f
Makcimus48 <32389239+Makcimus48@users.noreply.github.com>
2017-10-19 15:42:26 +0300

    AddSecondTask

---
+++ PythonRequest.py
@@ -0,0 +1,9 @@
+import requests
+import json
+data = {'event':'Make Python great again'}
+url = 'http://cit-home1.herokuapp.com/api/register'
+headers = {'content-type':'application/json'}
+r = requests.post(url, data=json.dumps(data), headers=headers)
+print(r.json()['answer'])
+r = requests.get('https://cit-home1.herokuapp.com/api/check_me')
+print(r.json())
