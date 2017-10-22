25cea422ff9d2bf7c788de6b5dfead4023650e5f
vadimbandel <vadim@bandel.ru>
2017-10-22 21:55:53 +0300

    add programm SimpleNumbers

---
+++ Registration.py
@@ -0,0 +1,10 @@
+import requests
+import json
+
+request = requests.post("http://cit-home1.herokuapp.com/api/register",
+                        data = json.dumps({'message':'Registration...'}),
+                        headers = {'content-type': 'application/json'})
+
+if request.status_code == 200:
+    request = requests.get("http://cit-home1.herokuapp.com/api/check_me")
+    print(request.json())
