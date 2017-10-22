d712d150aa95e390793d7bc08022cab21b84529e
vadimbandel <vadim@bandel.ru>
2017-10-22 22:00:17 +0300

    add programm Registration

--- Registration.py
+++ Registration.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 request = requests.post("http://cit-home1.herokuapp.com/api/register",
-                        data = json.dumps({'message':'Registration...'}),
+                        data = json.dumps({'message':'Registration process...'}),
                         headers = {'content-type': 'application/json'})
 
 if request.status_code == 200:
